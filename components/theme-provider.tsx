44708947349baf1e50bfda6cd105502948142e61
Varun Kilaru <kilaruvarun@gmail.com>
2025-04-29 23:08:15 -0400

    Added system

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,7 +2,7 @@
 
 import { createContext, useContext, useEffect, useState } from "react"
 
-type Theme = "dark" | "light"
+type Theme = "dark" | "light" | "system"
 
 type ThemeProviderProps = {
   children: React.ReactNode
